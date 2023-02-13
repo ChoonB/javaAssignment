6ccd03b457ca6815ad64e1c9261e88981e24d4dc
ChoonB <shgkdc@gmail.com>
2023-02-13 17:29:57 +0900

    마지막 문제 해결

--- day1/Report2.java
+++ day1/Report2.java
@@ -88,7 +88,7 @@ class Exercise3_5{
 class Exercise3_6{
     public static void main(String[] args){
         int fahrenheit = 100;
-        float celcius = ((float)5/9 * (fahrenheit-32));
+        float celcius = ((int)((5/9f * (fahrenheit-32))*100 + 0.5))/100F;
 
         System.out.println("Fahrenheit:"+fahrenheit);
         System.out.println("Celcius:"+celcius);
