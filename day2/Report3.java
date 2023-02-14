ab76c8910720715b32f4bacc0dd86b0054109243
ChoonB <shgkdc@gmail.com>
2023-02-14 17:29:05 +0900

    Report3에 4-4 해결!!

--- day2/Report3.java
+++ day2/Report3.java
@@ -62,29 +62,23 @@ class Exercise4_3 {
 
 //4-4. 1+(-2)+3+(-4)+...과 같은 식으로 계속 더해나갔을 때,
 //몇까지 더해야 총합이 100 이상이 되는지 구하세요.
-//class Exercise4_4 {
-//    public static void main(String[] args) {
-//        int sum = 0; // 총합을 저장할 변수
-//        int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
-//        int num = 0;
-////        roop:
-////        while (true) {
-////            if (num >= 0) {
-////                num = num + 1;
-////            } else {
-////                num = num - 1;
-////            }
-////            sum += num;
-////            if (sum >= 100) {
-////                break roop;
-////            }
-////            s = -s;
-////            num = s * num;
-////        }
-////        System.out.println(num);
-////        System.out.println(sum);
-////    }
-//}
+class Exercise4_4 {
+    public static void main(String[] args) {
+        int sum = 0; // 총합을 저장할 변수
+        int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
+        int num = 0;
+
+        while (sum<100) {
+            num = Math.abs(num) + 1;
+            num = num * s;
+            s = -s;
+            sum += num;
+
+        }
+        System.out.println(num);
+        System.out.println(sum);
+    }
+}
 
 
 
